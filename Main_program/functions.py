a589ea85e2d8f3e9983843a8d693cc4845a92e0d
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-07-22 23:48:53 -0300

    Add stock option, commented all, added colors to guide what is saved

--- Main_program/functions.py
+++ Main_program/functions.py
@@ -1,15 +1,21 @@
 import json
 from time import sleep
 session = [] #variable that will handle all the new registers
-explainingheader = f'{"Product Name":^28}|{"Distributor":^25}|{"Original Price (US$)":^22}|{"Discount (US$)":^19}|{"Final Price (US$)":^19}' #created just to avoid typing ALL again
+explainingheader = f'{"Product Name":^28}|{"Stock Quantity (Units)":^24}|{"Original Price (US$)":^22}|{"Discount (US$)":^18}|{"Final Price (US$)":^19}' #created just to avoid typing ALL again
 
+
+#informs that cant be pulled the content of the master file
+#if the program runs without reaching the old data, this appears
+#that will cause to erase the past data when trying to save the new informations
 def error_pulling():
         print('\033[31mBECAREFULL!!! \033[m')
         print('\033[31mWE CANNOT LOAD THE FILE\033[m')
         print('\033[31mTHE RETURN IS JUST A EMPTY LIST\033[m')
         print("\033[31mEXTREMELLY RECOMENDED TO DON'T USE THE APLICATTION\033[m")
         sleep(4)
-    
+
+
+#opens the main file and returns to a variable on the main program    
 def pull_file(filename):
     try:
         with open(filename, 'r') as file:
@@ -23,24 +29,25 @@ def pull_file(filename):
     
 
 #display the header with the message that you want
-def header(msg):
-    print('='*117)
+def header(msg, character='='):
+    print(character*117)
     print(f'{msg:^117}')
-    print('='*117)
+    print(character*117)
 
 
 #everytime the ctrl+c or the exit option be selected this will me trigered    
 #if ctrl+c be pressed again it imediatly ends without saving nothing
 #otherwise will asks if you wanna save or not
-#if the choice was to save, send all your sessions changes to a file
+#if the choice was to save, re-write the old data on the file to our varible that have everything that was on the file plus our new data
 #if its not, your discard everything you do in the session
 def wayout(loadedfile):
     print('\033[31mThe user decided to stop the program...\033[m')
+    print()
     print("\033[32m[1] Send the informations saved localy to the Database\033[m\n\033[31m[2] Exit without saving\033[m")
     while True:
         try:
             choice = int(input("What's your choice?: "))
-        except KeyboardInterrupt:
+        except KeyboardInterrupt: #stop imediatly
             print('\033[31mStopping the program right now...\033[m')
             exit()
         except:
@@ -49,14 +56,14 @@ def wayout(loadedfile):
             if choice in (1,2):
                 break
     if choice == 1:
-        save_on_file(loadedfile)
+        save_on_file(loadedfile) # re-write everything in the file
         exit()
     elif choice == 2:
         print('\033[31mNothing in this session was saved...\033[m')
         exit()
 
-
-#gonna save the inputs on the .json file
+#tries to re-write the main file with our varible that have all the old data + our new sessions saving
+#THINK LATER: if nothing was changed we dont need to write it all again on the file.
 def save_on_file(loadedfile, filename='data.json'):
     try:
         with open(filename, 'w') as file:
@@ -82,34 +89,36 @@ def validation(msg, loadedfile, convert_type=str):
             return choice
 
 
-#bassicaly try to register a series of things on a dictionary and later to a list if its all right     
+#try to register a series of inputs on a dictionary, if its all right append it to the loaded list from the main file     
 def new_products_register(loadedfile):
+    global session
     while True:
-        #first it will read all the informations and save on a dictionary
+        #first of all, get all the informations and save on a dictionary
         name = validation('Product name: ', loadedfile).strip()
+        stock = validation('What is the stock quantity right now?: ', loadedfile, convert_type=int)
         price = validation('Product price: US$', convert_type=float, loadedfile=loadedfile)
-        distributor = validation('What is the distributor?: ', loadedfile).strip()
         DiscountPercentage = validation('What percentage of discount will it have now?: ', convert_type=float, loadedfile=loadedfile)
         DiscountMoney = price*DiscountPercentage/100
         finalprice = price - DiscountMoney
-        LastTyped = {'Name': name, 'Distributor':distributor, 'Original Price':price, 'Discount Money':DiscountMoney, 'Discount Percentage':DiscountPercentage, 'Final Price':finalprice}
+        LastTyped = {'Name': name, 'Stock':stock, 'Original Price':price, 'Discount Money':DiscountMoney, 'Discount Percentage':DiscountPercentage, 'Final Price':finalprice}
         
-        #here the program will show the last things you wrote
+        #here the program will show the last things you wrote in a formated way
         print('='*117)
         print(explainingheader)        
-        print(f'{name:^28}|{distributor:^25}|{price:^22.2f}| {DiscountPercentage:<5}% - {DiscountMoney:<9.2f}|{finalprice:^19.2f}')
+        print(f'{name:^28}|{stock:^24}|{price:^22.2f}|{DiscountPercentage:>5}% - {DiscountMoney:<9.2f}|{finalprice:^19.2f}')
         print('='*117)
         
-        #if you agree it will save on a list, if you don't it will restart the process of typing
+        #if you agree it will save on loaded list from the main file, case you don't it will restart the process
         print('\033[32m[1] Save Locally\033[m\n\033[31m[2] Type all informations again\033[m')
         while True:
             choice = validation('Did you want to save localy those informations?: ', convert_type=int, loadedfile=loadedfile)
             if choice in (1,2):
                 break
         if choice == 1:
+            session.append(LastTyped)
             loadedfile.append(LastTyped)
             break
-        if choice == 2:
+        if choice == 2:  #maybe i can delete this 2 lines
             continue
 
 
@@ -117,33 +126,24 @@ def new_products_register(loadedfile):
 def show_past_registers(FileName):
     try:
         with open(FileName, 'r') as file:
-            stuff_inside = file
-    except KeyboardInterrupt:
-        wayout()
+            return json.load(file)
     except FileNotFoundError or FileExistsError: 
         print('We cannot find the file to show you, sorry') 
     except Exception as error:
-        print('I cant send you a list of your registers because of this')
+        print("I cant send you what you wan't because of this")
         print(error)
-    else:
-        if stuff_inside == None or stuff_inside == [] or stuff_inside == '' or stuff_inside == {}:
-            print('Nothing to show you unfortunatly')
-        else:
-            return stuff_inside
         
-#show all the things that are saved on the session var (the list that we put all the things on register function)
+#show all the things that are registered only in this session on red
 def show_session_saveds():
     global session
-    header(explainingheader)
-    for counter,item in enumerate(session):
+    for item in session:
         if item == []: 
             continue
         else:
-            print(f'{item["Name"]:^28}|{item["Distributor"]:^25}|{item["Original Price"]:^22.2f}| {item["Discount Percentage"]:<5}% - {item["Discount Money"]:<9.2f}|{item["Final Price"]:^19.2f}')
+            print(f'\033[31m{item["Name"]:^28}|{item["Stock"]:^24}|{item["Original Price"]:^22.2f}|{item["Discount Percentage"]:>5}% - {item["Discount Money"]:<9.2f}|{item["Final Price"]:^19.2f}\033[m')
             
             
-#show the products again, and alow you to change the stock quantity
-def change_file_stock():
-    show_past_registers()
+#Alow the user to change any information
+def update_data():
     pass
 
--- Main_program/main.py
+++ Main_program/main.py
@@ -1,34 +1,47 @@
 import functions
+from time import sleep
 filename = 'data.json'
 loadedfile = functions.pull_file(filename)
 while True:
     functions.header(' Welcome to our registering System ')
     print("""[1] Register new products
 [2] View current products
-[3] Set stock quantity
+[3] Update any information
 [4] Exit the program""")
     Main_Choice = functions.validation(msg="What's is your choice?: ", convert_type=int, loadedfile=loadedfile)
-    print('='*117)
     
-    
+    #Registering Area
     if Main_Choice == 1:
+        print('='*117)
         functions.new_products_register(loadedfile)
         
-        
+    #Showing all the past registers on green and the locals on red   
     elif Main_Choice == 2:
+        print()
+        functions.header('Stock Management', character='*')
         Second_Choice = functions.show_past_registers(filename)
         if Second_Choice == None or Second_Choice == [] or Second_Choice == '' or Second_Choice == {}:
-            continue
+            print('Nothing returned to us, sorry')
         else:
             print(functions.explainingheader)
             for item in Second_Choice:
-                print(f'{item["Name"]:^28}|{item["Distributor"]:^25}|{item["Original Price"]:^22.2f}| {item["Discount Percentage"]:<5}% - {item["Discount Money"]:<9.2f}|{item["Final Price"]:^19.2f}')
-                
-                
+                print(f'\033[32m{item["Name"]:^28}|{item["Stock"]:^24}|{item["Original Price"]:^22.2f}|{item["Discount Percentage"]:>5}% - {item["Discount Money"]:<9.2f}|{item["Final Price"]:^19.2f}\033[m')
+            functions.show_session_saveds()
+            print('*'*117)
+            sleep(2)
+        print()
+
+
+    #Changing any previous information           
     elif Main_Choice == 3:
-        functions.change_file_stock()
+        print('='*117)
+        functions.update_data()
         
     #DONE  
     elif Main_Choice == 4:
+        print('='*117)
         functions.wayout(loadedfile)
 
+
+#NEW FEATURES
+#allow the user to change any information
