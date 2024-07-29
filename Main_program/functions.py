9eb61eba282e3da4974c53998ffc4faa309fc7e1
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-07-29 20:38:03 -0300

    Make update a def, reorganize the functions.py and comment some more

--- Main_program/functions.py
+++ Main_program/functions.py
@@ -1,12 +1,18 @@
 import json
 from time import sleep
+from copy import deepcopy
+
 session = [] #variable that will handle all the new registers
-explainingheader = f'{"Nº":<3}|{"Product Name":^28}|{"Stock Quantity (Units)":^24}|{"Original Price (US$)":^22}|{"Discount (US$)":^20}|{"Final Price (US$)":^19}' #created just to avoid typing ALL again
 
-
-#informs that cant be pulled the content of the master file
-#if the program runs without reaching the old data, this appears
-#that will cause to erase the past data when trying to save the new informations
+#Those things was just created to me avoid typing it all again
+explainingheader = f'{"Nº":<3}|{"Product Name":^28}|{"Stock Quantity (Units)":^24}|{"Original Price (US$)":^22}|{"Discount (US$)":^20}|{"Final Price (US$)":^19}'
+Straight_Line = '-'*121
+def padronized_file_error(msg):
+    print()
+    header(f'\033[31m{msg}\033[m', character='*')
+    print()
+    sleep(2)
+#If the program runs having data on the file, but without reaching it that will cause to erase the past data when trying to save the new informations
 def error_pulling():
         print('\033[31mBECAREFULL!!! WE CANNOT LOAD THE FILE\033[m')
         print('\033[31mTHE RETURN IS JUST A EMPTY LIST\033[m')
@@ -14,6 +20,16 @@ def error_pulling():
         print("\033[31mWE EXTREMELLY RECOMMEND TO DON'T USE THE APLICATTION\033[m")
         print("\033[31mIF IS YOUR FIRST TIME USING THE APP, PLEASE DISCONSIDER THIS INFO\033[m")
         sleep(4)
+#display the header with the message that you want
+def header(msg, character='='):
+    print(character*121)
+    print(f'{msg:^121}')
+    print(character*121)
+
+
+
+#HERE STARTS THE REAL USAGE FUNCTIONS
+
 
 
 #opens the main file and returns to a variable on the main program    
@@ -29,66 +45,33 @@ def pull_file(filename, counter=0, error=True):
         if error and counter <= 0:    
             error_pulling()
         return []
-    
 
-#display the header with the message that you want
-def header(msg, character='='):
-    print(character*121)
-    print(f'{msg:^121}')
-    print(character*121)
-
-
-#everytime the ctrl+c or the exit option be selected this will me trigered    
-#if ctrl+c be pressed again it imediatly ends without saving nothing
-#otherwise will asks if you wanna save or not
-#if the choice was to save, re-write the old data on the file to our varible that has everything that was on the file plus our new data
-#if its not, your discard everything you do in the session
-def wayout(loadedfile):
-    print('\033[31mThe user decided to stop the program...\033[m')
-    print()
-    print("\033[32m[1] Send the informations saved localy to the File\033[m\n\033[31m[2] Exit without saving\033[m")
-    while True:
-        try:
-            choice = int(input("What's your choice?: "))
-        except KeyboardInterrupt: #stop imediatly
-            print('\033[31mStopping the program right now...\033[m')
-            exit()
-        except:
-            print('\033[31mERROR! Please type a valid option...\033[m')
-        else:
-            if choice in (1,2):
-                break
-    if choice == 1:
-        save_on_file(loadedfile) # re-write everything in the file
-        exit()
-    elif choice == 2:
-        print('\033[31mNothing in this session was saved...\033[m')
-        exit()
-
-#first put the session things on our variable that hold the entire file
-#if all goes right, tries to re-write the main file with our varible that have all the old data + our new sessions saving
+#append_session alows you to put the session things on our variable that hold the entire file
+#mainly tries to re-write the main file with our varible (loadedfile) that have all the old data + our new sessions saving
 #THINK LATER: if nothing was changed we dont need to write it all again on the file.
-def save_on_file(loadedfile, filename='data.json'):
+def push_file(loadedfile, filename='data.json', append_session=True):
     global session
-    try:
-        for item in session:
-            loadedfile.append(item)
-    except Exception as error:
-        print('\033[31mWe cannot save the session because of this error...\033[m')
-        print(error)
-    else:
+    if append_session:
         try:
-            with open(filename, 'w') as file:
-                json.dump(loadedfile, file, indent=2)
+            for item in session:
+                loadedfile.append(item)
         except Exception as error:
-            print('\033[31mNothing in this session was saved...\033[m')
+            print('\033[31mWe cannot save the session because of this error...\033[m')
             print(error)
         else:
+            print('\033[32mThe session items loaded corectly...\033[m')
             session.clear()
-            print('\033[32mThis session was completely saved...\033[m')
-        
-
-#validation process of inputs that triggers a function when ctrl+c or exit button is pressed
+    try:
+        with open(filename, 'w') as file:
+            json.dump(loadedfile, file, indent=2)
+    except Exception as error:
+        print('\033[31mNothing in this session was saved...\033[m')
+        print(error)
+    else:
+        print('\033[32mThis session was completely saved...\033[m')
+   
+    
+#Validates any input, if the user press ctrl+c trigger wayout def 
 def validation(msg, loadedfile, MaxLength=1, convert_type=str, percentage=False, options=False, start=1, stop=1):
     while True:
         try:
@@ -120,7 +103,6 @@ def validation(msg, loadedfile, MaxLength=1, convert_type=str, percentage=False,
 def new_products_register(loadedfile):
     global session
     while True:
-        
         #first of all, get the informations and save on a dictionary
         name = validation('Product name: ', loadedfile, MaxLength=28).strip()
         stock = validation('What is the stock quantity right now?: ', loadedfile, convert_type=int, MaxLength=7)
@@ -139,14 +121,13 @@ def new_products_register(loadedfile):
         print()
         
         #if you agree it will save on loaded list from the main file, case you don't it will restart the process
-        print('\033[32m[1] Save Locally\033[m\n\033[31m[2] Type all informations again\033[m')
-        while True:
-            choice = validation('Did you want to save localy those informations?: ', convert_type=int, loadedfile=loadedfile, MaxLength=1)
-            if choice in (1,2):
-                break
-        if choice == 1:
+        print('\033[32m[1] Save Locally\033[m\n\033[31m[2] Type all informations again\n[3] Exit the register process\033[m')
+        choice = validation('Did you want to save localy those informations?: ', convert_type=int, loadedfile=loadedfile, MaxLength=1, options=True, start=1, stop=3)
+        if choice == 3:
+            break
+        elif choice == 1:
             session.append(LastTyped)
-            print('-'*121)
+            print(Straight_Line)
             print()
             break
 
@@ -160,20 +141,19 @@ def print_session_saveds_red():
         else:
             print(f'\033[31m{cont:<3}|{item["Name"]:^28}|{item["Stock"]:^24}|{item["Original Price"]:^22.2f}|{item["Discount Percentage"]:>6}% - {item["Discount Money"]:<10.2f}|{item["Final Price"]:^19.2f}\033[m')
             
-            
+#show in green color all the thing that are registered on the file
 def print_file_data(loadedfile, msg):
     print()
     header(msg, character='-')
     if loadedfile == None or loadedfile == [] or loadedfile == '' or loadedfile == {}:
         print(f"\033[31mSorry, we can't reach the file, maybe it's because the file is empty\033[m")
-        print('-'*121)
-        print()
+        return 1
     else:
         print(explainingheader)
         for number, item in enumerate(loadedfile, start=1):
             print(f'\033[32m{number:<3}|{item["Name"]:^28}|{item["Stock"]:^24}|{item["Original Price"]:^22.2f}|{item["Discount Percentage"]:>6}% - {item["Discount Money"]:<10.2f}|{item["Final Price"]:^19.2f}\033[m')
         
-
+#Made to return an existing key on the dict and the base formatation length, used only in the updating def
 def op2_return(op):
     if op == 1:
         return 'Name', 28
@@ -183,3 +163,95 @@ def op2_return(op):
         return 'Original Price', 8
     if op == 4:
         return 'Discount Percentage', 5
+
+#function that show the all the previous registered data in the file and let the user choice one of them to change 
+def updating(loadedfile, filename='data.json'):
+    while True:
+        #First of all, prints the existing data
+        print_file_data(loadedfile, 'Registers on the system')
+        print(Straight_Line)
+        print()
+        
+        #What dict we will need to change
+        dict_position = validation("Which item did you wan't to change?: ", loadedfile, MaxLength=3, convert_type=int, options=True, start=1, stop=len(loadedfile)) - 1
+        print()
+        
+        #What item we will need to change
+        print(f"\033[32m[1] Name\n[2] Stock quantity\n[3] Original Price\n[4] Discount percentage\n[5] Exit the update\033[m")
+        op2 = validation("Which item did you wan't to change?: ", loadedfile, MaxLength=3, convert_type=int, options=True, start=1, stop=5)
+        if op2 == 5:
+            break
+        dict_item = op2_return(op2)
+        
+        #Making the changes
+        if dict_item[0] == 'Name':
+            new_value = validation('New value: ', loadedfile, MaxLength=dict_item[1], convert_type=str)
+        elif dict_item[0] == 'Discount Percentage':
+            new_value = validation('New value: ', loadedfile, MaxLength=dict_item[1], convert_type=float, percentage=True)
+        elif dict_item[0] == 'Original Price':
+            new_value = validation('New value: ', loadedfile, MaxLength=dict_item[1], convert_type=float)
+        elif dict_item[0] == 'Stock':
+            new_value = validation('New value: ', loadedfile, MaxLength=dict_item[1], convert_type=int)
+            
+        placeholder = deepcopy(loadedfile[dict_position])
+        placeholder[dict_item[0]] = new_value
+        DiscountMoney_placeholder = (placeholder['Original Price']*placeholder['Discount Percentage'])/100
+        FinalPrice_placeholder = placeholder['Original Price'] - DiscountMoney_placeholder 
+        placeholder['Discount Money'] = DiscountMoney_placeholder
+        placeholder['Final Price'] = FinalPrice_placeholder
+        
+        #Showing how the change looks 
+        print()
+        header('Showing the changes', character='-')
+        print(explainingheader)
+        print(f'\033[32m{'1  '}|{placeholder["Name"]:^28}|{placeholder["Stock"]:^24}|{placeholder["Original Price"]:^22.2f}|{placeholder["Discount Percentage"]:>6}% - {placeholder["Discount Money"]:<10.2f}|{placeholder["Final Price"]:^19.2f}\033[m')
+        print(Straight_Line)
+        print()
+        
+        #Asking if the user wants it or no
+        print('\033[32m[1] Update the file \033[m\n\033[31m[2] Type all informations again\n[3] Exit the update without saving\033[m')
+        Update_or_not = validation('What is your choice?: ', loadedfile, MaxLength=1, convert_type=int, options=True, start=1, stop=3)
+        if Update_or_not == 3:
+            break
+        elif Update_or_not == 2:
+            print('Restarting...')
+            continue
+        
+        #If the user wants, explain that those changes go directly to the main file and asks again
+        if Update_or_not == 1:
+            header('\033[31mPLEASE BE CAREFUL, THIS ACTIONS GOES DIRECTLY TO THE FILE\033[m')
+            print('\033[31m[1] YES\n[2] NO\033[m')
+            Confirmation_Update = validation('\033[31mAre you sure?: \033[m ',loadedfile, MaxLength=1, convert_type=int, options=True, start=1, stop=2)
+            if Confirmation_Update == 1: #if the choise was yes, remove the old and insert the new one 
+                del loadedfile[dict_position]
+                loadedfile.insert(dict_position, placeholder)
+                push_file(loadedfile, filename, append_session=False)
+                placeholder.clear()
+                break
+            if Confirmation_Update == 2:
+                print('Restarting...')
+
+
+#everytime the ctrl+c or the exit option be selected this will me trigered    
+def wayout(loadedfile):
+    print('\033[31mThe user decided to stop the program...\033[m')
+    print()
+    print("\033[32m[1] Send the informations saved localy to the File\033[m\n\033[31m[2] Exit without saving\033[m")
+    while True:
+        try:
+            #Gently asks if you wanna save or not
+            choice = validation("What's your choice?: ", loadedfile, MaxLength=1, convert_type=int, options=True, start=1, stop=2)
+        except KeyboardInterrupt: #if ctrl+c be pressed, it imediatly ends without saving nothing
+            print('\033[31mStopping the program right now...\033[m')
+            exit()
+        except:
+            print('\033[31mERROR! Please type a valid option...\033[m')
+        else:
+            if choice in (1,2):
+                break
+    if choice == 1:
+        push_file(loadedfile) # re-write everything in the file
+        exit()
+    elif choice == 2:
+        print('\033[31mNothing in this session was saved...\033[m')
+        exit()
--- Main_program/main.py
+++ Main_program/main.py
@@ -1,33 +1,34 @@
 import functions, json
 from time import sleep
 filename = 'data.json'
-functions.header(' Welcome to our registering System ')
 c = 0
 while True:
     loadedfile = functions.pull_file(filename, counter=c)
     c += 1
+    #Asksing for what to do
+    functions.header("Products registering system")
     print("""[1] Register new products
 [2] Show past registers
 [3] Update past registers
 [4] Save Session updates
 [5] Exit the program""")
-    
     Main_Choice = functions.validation(msg="What's is your choice?: ", convert_type=int, loadedfile=loadedfile, MaxLength=1, options=True, start=1, stop=5)
 
 
     #Registering Area
     if Main_Choice == 1:
-        print('-'*121)
+        print(functions.Straight_Line)
         functions.new_products_register(loadedfile)
         
 
     #Showing all the past registers on green and the locals on red   
     elif Main_Choice == 2:
-        functions.print_file_data(loadedfile, 'Stock Management')
+        legend = functions.print_file_data(loadedfile, 'Stock Management')
         functions.print_session_saveds_red()
-        print('-'*121)
-        print('\033[32mGreen represents the saved data\033[m')
-        print('\033[31mRed represents the sessions data\033[m')
+        print(functions.Straight_Line)
+        if legend != 1:
+            print('\033[32mGreen represents the saved data\033[m')
+            print('\033[31mRed represents the sessions data\033[m')
         print()
         sleep(2)
 
@@ -35,104 +36,32 @@ while True:
     elif Main_Choice == 3:
         #if theres nothing on the file stops, but if it finds something ask the user what they want to do
         if loadedfile == None or loadedfile == [] or loadedfile == '' or loadedfile == {}:
-            continue
+            functions.padronized_file_error("Sorry, we cannot find anything on the file to update")
         else:
-            #taking the informations
-            while True:
-                loadedfile = functions.pull_file(filename, counter=c)
-                functions.print_file_data(loadedfile, 'Changing Section')
-                print('-'*121)
-                print()
-                #What dict we will need to change
-                dict_position = functions.validation("Which item did you wan't to change?: ", loadedfile, MaxLength=3, convert_type=int, options=True, start=1, stop=len(loadedfile)) - 1
-                print()
-                
-                
-                #What item we will need to change
-                print(f"\033[32m[1] Name\n[2] Stock quantity\n[3] Original Price\n[4] Discount percentage\033[m")
-                op2 = functions.validation("Which item did you wan't to change?: ", loadedfile, MaxLength=3, convert_type=int, options=True, start=1, stop=4)
-                dict_item = functions.op2_return(op2)
-                
-                
-                #Making the changes
-                if dict_item[0] == 'Name':
-                    new_value = functions.validation('New value: ', loadedfile, MaxLength=dict_item[1], convert_type=str)
-                elif dict_item[0] == 'Discount Percentage':
-                    new_value = functions.validation('New value: ', loadedfile, MaxLength=dict_item[1], convert_type=float, percentage=True)
-                elif dict_item[0] == 'Original Price':
-                    new_value = functions.validation('New value: ', loadedfile, MaxLength=dict_item[1], convert_type=float)
-                elif dict_item[0] == 'Stock':
-                    new_value = functions.validation('New value: ', loadedfile, MaxLength=dict_item[1], convert_type=int)
-                    
-                placeholder = loadedfile[dict_position]
-                placeholder[dict_item[0]] = new_value
-                DiscountMoney_placeholder = (placeholder['Original Price']*placeholder['Discount Percentage'])/100
-                FinalPrice_placeholder = placeholder['Original Price'] - DiscountMoney_placeholder 
-                placeholder['Discount Money'] = DiscountMoney_placeholder
-                placeholder['Final Price'] = FinalPrice_placeholder
-                
-                
-                #Showing how the change look and asking if the user wants it or no     
-                print()
-                functions.header('Showing the changes', character='-')
-                print(functions.explainingheader)
-                print(f'\033[32m{'1  '}|{placeholder["Name"]:^28}|{placeholder["Stock"]:^24}|{placeholder["Original Price"]:^22.2f}|{placeholder["Discount Percentage"]:>6}% - {placeholder["Discount Money"]:<10.2f}|{placeholder["Final Price"]:^19.2f}\033[m')
-                print('-'*121)
-                print()
-                print('\033[32m[1] Update the file \033[m\n\033[31m[2] Type all informations again\033[m')
-                pl = functions.validation('What is your choice?: ', loadedfile, MaxLength=1, convert_type=int, options=True, start=1, stop=2)
-                if pl == 2:
-                    print('Restarting...')
-                    continue
-                if pl == 1:
-                    functions.header('\033[31mPLEASE BE CAREFUL\033[m')
-                    print('\033[31m[1] YES\n[2] NO\033[m')
-                    pl2 = functions.validation('\033[31mThis action will save it directly to the file, Are you sure?:\033[m ',loadedfile, MaxLength=1, convert_type=int, options=True, start=1, stop=2)
-                    if pl2 == 1:
-                        del loadedfile[dict_position]
-                        loadedfile.insert(dict_position, placeholder)
-                        try:
-                            with open(filename, 'w') as file:
-                                json.dump(loadedfile, file, indent=2)
-                        except Exception as error:
-                            print('\033[31mNothing in this session was saved...\033[m')
-                            print(error)
-                        else:
-                            placeholder.clear()
-                            print('\033[32mThis session was completely saved...\033[m')
-                            break
-                    if pl2 == 2:
-                        print('Restarting...')
-                        continue
+            functions.updating(loadedfile, filename)
 
 
     #A way to save the session data on the file without needing to exit the program    
     elif Main_Choice == 4:
         if functions.session == []:
-            print()
-            functions.header("We don't have any data on current sesion to be saved.", character='*')
-            print()
-            sleep(2)
+            functions.padronized_file_error("We don't have any data on current sesion to be saved")
         else:
             print()
             functions.header(f'{"Session data":^121}', character='-')
             print(functions.explainingheader)
             functions.print_session_saveds_red()
-            print('-'*121)
+            print(functions.Straight_Line)
             print()
             print("\033[32m[1] Send the informations saved localy to the File\033[m\n\033[31m[2] Send all later\033[m")
             choice = functions.validation("What is your choice?: ", loadedfile=loadedfile, MaxLength=1, convert_type=int, options=True, start=1, stop=2)
             if choice == 1:
-                functions.save_on_file(loadedfile=loadedfile, filename=filename)
+                functions.push_file(loadedfile=loadedfile, filename=filename)
         
         
     #DONE  
     elif Main_Choice == 5:
-        print('='*121)
+        print(functions.Straight_Line)
         functions.wayout(loadedfile)
 
-    functions.header("Products Register")
-
-#FIX THE ERROR: Na opção de update, mesmo colocando que quero digitar as informações novamente ele salva na loadedfile a mudança
-#parcialmente resolvi fazendo o arquivo dar pull a cada loop, mas suspeito que eu tenha feito uma ligação da loadedfile com placeholder
 #THINK LATER: Do we really need to save that many informations on the file?
+#THINK LATER: if nothing was changed we dont need to write it all again on the file. functions.py LINE 112
